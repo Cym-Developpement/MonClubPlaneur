4dcbf4c752077657dfde6ddc8b60c28b6cf15564
Yann Challet <yann@cymdev.com>
2026-02-25 11:17:05 +0100

    test: vérifie le déploiement automatique par webhook

--- app/Http/Controllers/GitWebhookController.php
+++ app/Http/Controllers/GitWebhookController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class GitWebhookController extends Controller
 {
+
     public function update(Request $request)
     {
         // Vérification de la signature GitHub
