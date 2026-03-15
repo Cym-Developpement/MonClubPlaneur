f7b997faa1cfbde2cb4811852f474728d6043a54
Yann <yann@cymdev.com>
2026-03-15 13:15:05 +0100

    Revert "fix(pwa): cache-bust icônes et session longue"
    This reverts commit 4d5e43cf3450992197a177e5bb4985e4068fdc27.

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,6 @@ Route::get('/manifest.json', function () {
     $name      = \App\Models\parametre::getValue('club-nom_complet', config('app.name', 'MonClubPlaneur'));
     $shortName = \App\Models\parametre::getValue('club-nom_court', 'Club');
 
-    $v = file_exists(public_path('icons/icon-192x192.png'))
-        ? '?v=' . filemtime(public_path('icons/icon-192x192.png'))
-        : '';
-
     return response()->json([
         'name'             => $name,
         'short_name'       => $shortName,
@@ -33,16 +29,14 @@ Route::get('/manifest.json', function () {
         'background_color' => '#f8fafc',
         'icons'            => [
             [
-                'src'     => '/icons/icon-192x192.png' . $v,
-                'sizes'   => '192x192',
-                'type'    => 'image/png',
-                'purpose' => 'any',
+                'src'   => '/icons/icon-192x192.png',
+                'sizes' => '192x192',
+                'type'  => 'image/png',
             ],
             [
-                'src'     => '/icons/icon-512x512.png' . $v,
-                'sizes'   => '512x512',
-                'type'    => 'image/png',
-                'purpose' => 'any',
+                'src'   => '/icons/icon-512x512.png',
+                'sizes' => '512x512',
+                'type'  => 'image/png',
             ],
         ],
     ])->header('Content-Type', 'application/manifest+json');
